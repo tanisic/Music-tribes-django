ea5e8692878fa3676adbccd0535bce178223e3a0
Tin Anisic <tin.anisic@hotmail.com>
2021-01-23 11:13:34 +0100

    Add index.html and base.html

--- musicTribes/app/views.py
+++ musicTribes/app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    return render(request,"app/index.html",None)
