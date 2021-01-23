68c4a230a81f4c3097776a2d888adf3b5c18440c
Tin Anisic <tin.anisic@hotmail.com>
2021-01-23 11:59:52 +0100

    Add models: Tribe, Playlist, Song, Playlist, Comment, Like in models.py

--- musicTribes/app/models.py
+++ musicTribes/app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models import constraints
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class TimeStamped(models.Model):
     created_at = models.DateTimeField(editable=False)
@@ -16,3 +18,44 @@ class TimeStamped(models.Model):
         abstract = True
 
 class Tribe(TimeStamped):
+    chieftain=models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def chieftain(self):
+        return self.chieftain
+
+    def created_by(self):
+        return self.chieftain.username
+
+class Playlist(TimeStamped):
+    tribe = models.ForeignKey(Tribe,on_delete=models.CASCADE)
+    name = models.CharField(unique=True,blank=False,max_length=128)
+    creator = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
+
+    def str(self):
+        return self.name
+
+class Song(TimeStamped):
+    playlists = models.ManyToManyField(Playlist)
+    url = models.CharField(max_length=60,unique=True,blank=False)
+    title = models.CharField(blank=False,max_length=200)
+
+    def str(self):
+        return self.title
+class Comment(TimeStamped):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    playlist = models.ForeignKey(Playlist,on_delete=models.CASCADE)
+    text = models.TextField(blank=False)
+
+class Like(TimeStamped):
+    song = models.ForeignKey(Song,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    like = models.BooleanField(null=False,default=True)
+
+    class Meta:
+        constraints=[
+            models.UniqueConstraint(name='user_like',fields=['user','song'])
+        ]
+
+    def liked(self):
+        return self.like
+
