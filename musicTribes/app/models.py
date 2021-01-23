e0e3e8c4bed1e4f10f23e0e9a9d323eea0310147
rrajsic <cisjarnibor@gmail.com>
2021-01-23 11:22:27 +0100

    add timestamp model

--- musicTribes/app/models.py
+++ musicTribes/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class TimeStamped(models.Model):
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = timezone.now()
+
+        self.updated_at = timezone.now()
+        return super(TimeStamped, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
