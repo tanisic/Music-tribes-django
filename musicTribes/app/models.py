75316ac4535aa9bcd8f19dfab8f9621b17952bcf
rrajsic <cisjarnibor@gmail.com>
2021-02-03 11:07:14 +0100

    add avatar to tribes

--- musicTribes/app/models.py
+++ musicTribes/app/models.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
+    bio = models.TextField(max_length=1000, blank=True)
     avatarurl =  models.CharField(max_length=500,blank=False)
 
     def __str__(self):
@@ -39,6 +39,8 @@ class TimeStamped(models.Model):
 class Tribe(TimeStamped):
     creator = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     name = models.CharField(unique=True,max_length=50,blank=False)
+    logourl =  models.CharField(max_length=1000,blank=False)
+    genre = models.CharField(max_length=100,blank=False)
     def author(self):
         return self.creator
 
